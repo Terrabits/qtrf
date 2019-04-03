b964b2f1ac939cf6b52058115cf8ea62cc3f05fe
Nick Lalic <nick.lalic@gmail.com>
2019-04-02 22:36:44 -0700

    :construction: Changed dimensions of FileWidget UI

--- qtrf/widgets/file/ui_filewidget.py
+++ qtrf/widgets/file/ui_filewidget.py
@@ -3,8 +3,8 @@
 # Form implementation generated from reading ui file 'designer/filewidget.ui',
 # licensing of 'designer/filewidget.ui' applies.
 #
-# Created: Tue Apr  2 20:02:27 2019
-#      by: pyside2-uic  running on PySide2 5.12.2
+# Created: Tue Apr  2 21:31:59 2019
+#      by: pyside2-uic  running on PySide2 5.12.0
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -13,21 +13,41 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_FileWidget(object):
     def setupUi(self, FileWidget):
         FileWidget.setObjectName("FileWidget")
-        FileWidget.resize(133, 33)
+        FileWidget.resize(131, 22)
         self.horizontalLayout = QtWidgets.QHBoxLayout(FileWidget)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.filename = QtWidgets.QLineEdit(FileWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.filename.sizePolicy().hasHeightForWidth())
+        self.filename.setSizePolicy(sizePolicy)
         self.filename.setReadOnly(True)
         self.filename.setObjectName("filename")
         self.horizontalLayout.addWidget(self.filename)
         self.open = QtWidgets.QPushButton(FileWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.open.sizePolicy().hasHeightForWidth())
+        self.open.setSizePolicy(sizePolicy)
+        self.open.setMinimumSize(QtCore.QSize(60, 0))
+        self.open.setMaximumSize(QtCore.QSize(60, 16777215))
         self.open.setObjectName("open")
         self.horizontalLayout.addWidget(self.open)
         self.clear = QtWidgets.QPushButton(FileWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.clear.sizePolicy().hasHeightForWidth())
+        self.clear.setSizePolicy(sizePolicy)
+        self.clear.setMinimumSize(QtCore.QSize(60, 0))
+        self.clear.setMaximumSize(QtCore.QSize(60, 16777215))
         self.clear.setObjectName("clear")
         self.horizontalLayout.addWidget(self.clear)
+        self.horizontalLayout.setStretch(0, 1)
 
         self.retranslateUi(FileWidget)
         QtCore.QMetaObject.connectSlotsByName(FileWidget)
