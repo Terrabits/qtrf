4f190815cdad2997649f88b3759dbabad0f785c1
Nick Lalic <nick.lalic@gmail.com>
2020-03-26 13:56:52 -0700

    ✨ added value_changed signal to labels/edits.

--- qtrf/mixins/value.py
+++ qtrf/mixins/value.py
@@ -1,21 +1,34 @@
-from decimal           import Decimal
+from PySide2.QtCore    import Signal
 from qtrf.units        import prefix_map, to_decimal, to_str
 
-# Requires text(), setText(text) methods
-# to exist in inheriting class
+
+# Use requires:
+# - class be QWidget
+# - methods: text() and setText(text)
 class ValueMixin(object):
     def __init__(self):
         self.decimal_places = 3
         self.prefix_map     = prefix_map.copy()
         self.units          = 'U'
+
+    # value
     @property
     def value(self):
         return to_decimal(self.text(), self.units)
+
     @value.setter
     def value(self, value):
+        if self.value == value:
+            return
         if value == None or value == '':
             self.clear()
-            return
-        self.setText(self._to_str(value))
+        else:
+            self.setText(self._to_str(value))
+        self.value_changed.emit(self.value)
+
+    # signals
+    value_changed = Signal(float)
+
+    # helper
     def _to_str(self, value):
         return to_str(value, self.decimal_places, self.prefix_map, self.units)
