481c5c8f8f57dc4bbc62594f9af8830c8fab3408
Nick Lalic <nick.lalic@gmail.com>
2021-06-03 16:57:25 -0600

    🐛 generally speaking, meters are not expected to be negative

--- qtrf/widgets/meters/line_edit.py
+++ qtrf/widgets/meters/line_edit.py
@@ -4,6 +4,7 @@ from qtrf.widgets.value.line_edit import ValueLineEdit
 class MetersLineEdit(ValueLineEdit):
     def __init__(self, parent=None):
         ValueLineEdit.__init__(self, parent)
-        self.prefix_map = prefix_map
-        self.units      = units
+        self.prefix_map       = prefix_map
+        self.include_negative = False
+        self.units            = units
         self.update_validation()
