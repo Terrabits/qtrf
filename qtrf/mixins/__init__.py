0355a12ab3d1db6c4fce3d659048a54a18b4dddc
Nick Lalic <nick.lalic@gmail.com>
2021-07-15 16:12:58 -0600

    🐛 IntValueMixin should be included in export

--- qtrf/mixins/__init__.py
+++ qtrf/mixins/__init__.py
@@ -1,5 +1,6 @@
 from .float_line_edit import FloatLineEditMixin
 from .float_value     import FloatValueMixin
 from .int_line_edit   import IntLineEditMixin
+from .int_value       import IntValueMixin
 from .prefix_keys     import PrefixKeysMixin
 from .special_keys    import SpecialKeysMixin
